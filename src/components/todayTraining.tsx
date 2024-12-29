21743d46601a7feaac8945132142acd7e4abb466
Eduardo da Silva <eduardosilva.pn@gmail.com>
2024-12-29 17:23:29 -0300

    TodayTraining addition

---
+++ src/components/todayTraining.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { View } from "react-native";
+import { useSelector } from "../store";
+import { Training, Weekdays } from "../types";
+import moment from "moment";
+import { Card, Text } from "react-native-paper";
+
+export default function TodayTraining() {
+  const trainingList = useSelector((s) => s.calendar.trainingList);
+  const [todayTraining, setTodayTraining] = useState<Array<Training>>([]);
+
+  useEffect(() => {
+    const today: Weekdays =
+      Weekdays[moment().format("dddd") as keyof typeof Weekdays];
+    const todayTrainingList = trainingList.filter((v) => v.weekday === today);
+    setTodayTraining(todayTrainingList ? todayTrainingList : []);
+  }, [trainingList]);
+
+  return (
+    <View style={{flexDirection: "column", gap: 5}}>
+      <Text variant="titleMedium">Today Training</Text>
+      {todayTraining.map((training) => (
+        <Card>
+          <Card.Content>
+            <Text>{training.name}</Text>
+          </Card.Content>
+        </Card>
+      ))}
+    </View>
+  );
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,10 @@
 import { View } from "react-native";
-import { Text } from "react-native-paper";
+import TodayTraining from "../components/todayTraining";
 
 export default function Home() {
   return (
     <View>
-      <Text variant="bodyLarge">Home</Text>
+      <TodayTraining />
     </View>
   );
 }
